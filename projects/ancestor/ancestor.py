6afdb11c9fa6d2bc81dffa7048d7cdddce7c0c0c
Rasha Moumneh <rasha@fastmail.com>
2020-04-14 22:05:48 -0400

    psuedocode

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -4,13 +4,24 @@ from graph import Graph
 def earliest_ancestor(ancestors, starting_node):
     # turn the dataset into a graph by looping over the tuples in the array, and passing in the first item in the tuple using add_vertex, and the second item using add_edge
     graph = Graph()
-    for ancestor in ancestors:
-        for vertex in ancestor:
-            if vertex not in graph.vertices:
-                graph.add_vertex(vertex)
-        graph.add_edge(ancestor[0], ancestor[1])
-    # we now have a graph.
-    # do a depth first traversal using the starting node
+    # for ancestor in ancestors:
+    #     for vertex in ancestor:
+    #         if vertex not in graph.vertices:
+    #             graph.add_vertex(vertex)
+    #     graph.add_edge(ancestor[0], ancestor[1])
+    for pair in ancestors:
+        parent = pair[0]
+        child = pair[1]
+        if parent not in graph.vertices:
+            graph.add_vertex(parent)
+        if child not in graph.vertices:
+            graph.add_vertex(child)
+        # we're doing child: parent because we're traversing from bottom to top
+        graph.add_edge(child, parent)
+    print(graph.vertices)
+
+    # do depth first search with starting node, and return all the possible paths that end in None
+    # compare the paths, and return the one with the longest length
 
 
 # we need to find the vertex at the farthest path from the individual - depth first search
@@ -29,4 +40,4 @@ test_ancestors = [
     (10, 1),
 ]
 
-earliest_ancestor(test_ancestors, 4)
+earliest_ancestor(test_ancestors, 7)
