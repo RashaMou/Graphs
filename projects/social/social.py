37260b52101f98bc925a976d016338b830e5f987
Rasha Moumneh <rasha@fastmail.com>
2020-04-15 14:01:42 -0400

    populate graph

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,9 +1,14 @@
+import random
+
+
 class User:
     def __init__(self, name):
         self.name = name
 
+
 class SocialGraph:
     def __init__(self):
+        # id of the last added user
         self.last_id = 0
         self.users = {}
         self.friendships = {}
@@ -14,7 +19,10 @@ class SocialGraph:
         """
         if user_id == friend_id:
             print("WARNING: You cannot be friends with yourself")
-        elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
+        elif (
+            friend_id in self.friendships[user_id]
+            or user_id in self.friendships[friend_id]
+        ):
             print("WARNING: Friendship already exists")
         else:
             self.friendships[user_id].add(friend_id)
@@ -44,9 +52,23 @@ class SocialGraph:
         self.friendships = {}
         # !!!! IMPLEMENT ME
 
-        # Add users
+        # Add users using the add_user num_user times
 
-        # Create friendships
+        # Create random friendships
+        for i in range(0, num_users):
+            self.add_user(f"User {i + 1}")
+        # generate all friendship combinations
+        possible_friendships = []
+        # avoid duplicates by making sure first number is smaller than second
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+        # shuffle all possible friendships
+        random.shuffle(possible_friendships)
+        # create for first x pairs, where x is half the number of friends, because each connection creates two friendships
+        for i in range((num_users * avg_friendships) // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
@@ -62,9 +84,9 @@ class SocialGraph:
         return visited
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sg = SocialGraph()
     sg.populate_graph(10, 2)
     print(sg.friendships)
-    connections = sg.get_all_social_paths(1)
-    print(connections)
+    # connections = sg.get_all_social_paths(1)
+    # print(connections)
